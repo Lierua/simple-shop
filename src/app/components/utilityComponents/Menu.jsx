02d6d7a0b62c48928bbdc5307b44eb0599d53941
Oliver <oliverbengtsson67@gmail.com>
2025-11-14 08:53:00 +0100

    ++

--- src/app/components/utilityComponents/Menu.jsx
+++ src/app/components/utilityComponents/Menu.jsx
@@ -1,19 +1,26 @@
 import { IoHomeOutline } from "react-icons/io5";
 import { SlBasket } from "react-icons/sl";
 import { AiOutlineShopping } from "react-icons/ai";
+import Link from "next/link";
 
 const Menu = () => {
   return (
     <div className="flex gap-[4rem] py-6">
-      <h1 className="flex gap-2">
-        Simple Shop <IoHomeOutline className="scale-[1.3]" />
-      </h1>
-      <ul className="flex gap-8 [&>p]:mr-4 [&>p]:font-semibold [&>li]:flex [&>li]:gap-2">
+      <Link href="/">
+        <h1 className="flex gap-2">
+          Simple Shop <IoHomeOutline className="scale-[1.3]" />
+        </h1>
+      </Link>
+      <ul className="flex gap-8 [&>p]:mr-4 [&>p]:font-semibold ">
         <li>
-          <p>Products</p> <AiOutlineShopping className="scale-[1.3]" />
+          <Link href="/products" className="flex gap-2">
+            <p>Products</p> <AiOutlineShopping className="scale-[1.3]" />
+          </Link>
         </li>
         <li>
-          <p>Basket</p> <SlBasket className="scale-[1.3]" />
+          <Link href="/" className="flex gap-2">
+            <p>Basket</p> <SlBasket className="scale-[1.3]" />
+          </Link>
         </li>
       </ul>
     </div>
