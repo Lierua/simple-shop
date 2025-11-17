6631fedc98c8d49e94220a9c4bc832e76a304c2c
solom0001 <somw0001@stud.kea.dk>
2025-11-17 17:46:29 +0100

    card styling

--- src/app/components/detailComponents/BasketButton.jsx
+++ src/app/components/detailComponents/BasketButton.jsx
@@ -11,12 +11,8 @@ const BasketButton = ({ product }) => {
   return (
     <button
       onClick={() => addItem(product)}
-      className={`${
-        isInBasket
-          ? "bg-(--secondary) text-(--primary)"
-          : "hover:bg-(--secondary) hover:text-(--primary)"
-      } 
-      transition-all duration-200 ease mt-auto mb-4 mr-auto px-[25px] py-[8px] border-white border-[2px] rounded-[5px]`}
+      className={`${isInBasket ? "bg-secondary text-primary" : "hover:bg-(--secondary) hover:text-(--primary)"} 
+      transition-all duration-200 ease mt-auto mb-4 mr-auto px-[25px] py-[8px] border-white border-[2px] rounded-[5px] bg-blue text-primary`}
     >
       {isInBasket ? "Added" : "Add to cart"}
     </button>
